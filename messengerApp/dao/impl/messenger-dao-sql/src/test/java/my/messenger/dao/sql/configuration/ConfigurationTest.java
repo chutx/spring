804b1325ef4b4949f2112c96023c2de69dff8859
angelo <monstruito1@gmail.com>
2014-06-08 23:56:35 -0500

    adding configuration for testing of neo4j

--- messengerApp/dao/impl/messenger-dao-sql/src/test/java/my/messenger/dao/sql/configuration/ConfigurationTest.java
+++ messengerApp/dao/impl/messenger-dao-sql/src/test/java/my/messenger/dao/sql/configuration/ConfigurationTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(value={"classpath:META-INF/spring/orm-context.xml"})
+@ContextConfiguration(value={"classpath:META-INF/spring/tx-sql-context-test.xml"})
 public class ConfigurationTest {
 
 	@Autowired
